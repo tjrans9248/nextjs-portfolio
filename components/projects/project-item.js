f0dadfa0c02610fd1e1364713897f31d9f05ee83
tjrans9248 <a900906@naver.com>
2023-01-09 20:20:30 +0900

    add: blog 추가

--- components/projects/project-item.js
+++ components/projects/project-item.js
@@ -4,6 +4,7 @@ export default function ProjectItem({ data }) {
   const title = data.properties.Name.title[0].plain_text;
   const github = data.properties.Github.url;
   const youtube = data.properties.Youtube.url;
+  const blog = data.properties.Blog.url;
   const des = data.properties.Description.rich_text[0].plain_text;
   const imgSrc = data.cover.file?.url || data.cover.external.url;
   const tags = data.properties.Tags.multi_select;
@@ -54,6 +55,7 @@ export default function ProjectItem({ data }) {
         <h3 className="mt-4 text-xl">{des}</h3>
         <a href={github}>깃허브 바로가기</a>
         <a href={youtube}>유튜브 바로가기</a>
+        <a href={blog}>블로그 바로가기</a>
         <p className="my-1 ">
           작업기간 : {start} ~ {end} ({calculatedPeriod(start, end)}일)
         </p>
